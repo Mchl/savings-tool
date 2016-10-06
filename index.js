e3a966a0e04384df10d00db2639b46d7d7343d73
Michal Jarosz <michal.jarosz@stepstone.com>
2016-10-06 21:39:14 +0200

    Move funds data to a separate module

--- index.js
+++ index.js
@@ -3,73 +3,8 @@
 const dateFormat = (date) => `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`
 
 const https = require('https')
+const funds = require('./funds')
 
-const funds = [
-    {
-        id : 1,
-        name : 'NN Akcji'
-    }, {
-        id : 27,
-        name : 'NN (L) Japonia'
-    }, {
-        id : 1500037,
-        name : 'NN (L) Stabilny Globalnej Alokacji'
-    }, {
-        id : 18,
-        name : 'NN (L) Globalny Spółek Dywidendowych'
-    }, {
-        id : 20,
-        name : 'NN (L) Europejski Spółek Dywidendowych'
-    }, {
-        id : 3,
-        name : 'NN Obligacji'
-    }, {
-        id : 24,
-        name : 'NN (L) Globalny Długu Korporacyjnego'
-    }, {
-        id : 17,
-        name : 'NN Akcji Środkowoeuropejskich'
-    }, {
-        id : 8,
-        name : 'NN Gotówkowy'
-    }, {
-        id : 4,
-        name : 'NN Stabilnego Wzrostu'
-    }, {
-        id : 15,
-        name : 'NN Lokacyjny Plus'
-    }, {
-        id : 2,
-        name : 'NN Zrównoważony'
-    }, {
-        id : 34,
-        name : 'NN Perspektywa 2020'
-    }, {
-        id : 16,
-        name : 'NN Perspektywa 2025'
-    }, {
-        id : 33,
-        name : 'NN Perspektywa 2030'
-    }, {
-        id : 32,
-        name : 'NN Perspektywa 2035'
-    }, {
-        id : 31,
-        name : 'NN Perspektywa 2040'
-    }, {
-        id : 26,
-        name : 'NN Perspektywa 2045'
-    }, {
-        id : 19,
-        name : 'NN (L) Spółek Dywidendowych USA'
-    }, {
-        id : 22,
-        name : 'NN (L) Spółek Dywidendowych Rynków Wschodzących'
-    }, {
-        id : 29,
-        name : 'NN (L) Obligacji Rynków Wschodzących (Waluta Lokalna)'
-    }
-]
 
 const fundById = (id) => funds.find(fund => id === fund.id)
 
