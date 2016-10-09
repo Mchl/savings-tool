8e0c028b86af952bb104ed7a5832d28525086f4d
Michal Jarosz <michal.jarosz@stepstone.com>
2016-10-09 22:25:57 +0200

    Promisify google sheets api

--- google_sheets.js
+++ google_sheets.js
@@ -1,15 +1,19 @@
 const send = (auth, config, data) => {
-    const sheets = require('googleapis').sheets('v4')
-    sheets.spreadsheets.values.update({
-        auth,
-        spreadsheetId: config.googleSpreadsheet.spreadsheetId,
-        range: `${config.googleSpreadsheet.sheetName}!A1`,
-        valueInputOption: 'USER_ENTERED',
-        resource: {
+    return new Promise((resolve, reject) => {
+        const sheets = require('googleapis').sheets('v4')
+        sheets.spreadsheets.values.update({
+            auth,
+            spreadsheetId: config.googleSpreadsheet.spreadsheetId,
             range: `${config.googleSpreadsheet.sheetName}!A1`,
-            majorDimension: 'ROWS',
-            values: data
-        }
+            valueInputOption: 'USER_ENTERED',
+            resource: {
+                range: `${config.googleSpreadsheet.sheetName}!A1`,
+                majorDimension: 'ROWS',
+                values: data
+            }
+        }, reject)
+
+        resolve(true)
     })
 }
 
