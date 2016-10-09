addb764dbed9cd0bed154363b4b97839524639a1
Michal Jarosz <michal.jarosz@stepstone.com>
2016-10-09 22:26:19 +0200

    Promisify program flow

--- index.js
+++ index.js
@@ -8,109 +8,134 @@ const utils = require('./utils')
 const authorize = require('./google_authorize').authorize
 const googleApiKey = require(config.googleSpreadsheet.apiKeyFile)
 
-const sendData = (auth, data) => {
-    console.log(new Date())
-    const values = data.sort((v1,v2) => {
-        if (v1.name > v2.name) {
-            return 1
-        } else if (v1.name < v2.name) {
-            return -1
-        } else {
-            return 0
-        }
+const prepareData = (data) => {
+    return new Promise((resolve) => {
+        const values = data.sort((v1,v2) => {
+            if (v1.name > v2.name) {
+                return 1
+            } else if (v1.name < v2.name) {
+                return -1
+            } else {
+                return 0
+            }
+        })
+        .map(data => [
+            data.date,
+            data.name,
+            data.value,
+            data.change
+        ])
+
+        resolve(values)
     })
-    .map(data => [
-        data.date,
-        data.name,
-        data.value,
-        data.change
-    ])
-
-    require('./gmail').send(auth, config, values)
-    require('./google_sheets').send(auth, config, values)
-
 }
 
 let latestDataTimestamp = 0
 let shouldSendData = false
 
-const fetchData = (auth) => {
-    const groupedIds = funds.reduce((accumulator, current) => {
-        if (accumulator[accumulator.length - 1].length >= 4) {
-            accumulator.push([])
-        }
-        accumulator[accumulator.length - 1].push(current.id)
+const fetchData = () => {
+    console.log(new Date(), 'fetching data')
+    return new Promise((resolve, reject) => {
+        const groupedIds = funds.reduce((accumulator, current) => {
+            if (accumulator[accumulator.length - 1].length >= 4) {
+                accumulator.push([])
+            }
+            accumulator[accumulator.length - 1].push(current.id)
 
-        return accumulator
-    }, [[]])
+            return accumulator
+        }, [[]])
 
-    let receivedData = []
-    let requestsInProgress = 0
-    let result = []
+        let receivedData = []
+        let requestsInProgress = 0
+        let result = []
 
-    groupedIds.forEach((group, index) => {
-        const options = Object.assign({}, config.requestOptions, {
-            path: config.requestOptions.path + group.join()
-        })
-
-        requestsInProgress++
-
-        const request = https.request(options, res => {
-            res.on('data', (d) => {
-                if (receivedData[index] === undefined) {
-                    receivedData[index] = ''
-                }
-                receivedData[index] += d
+        groupedIds.forEach((group, index) => {
+            const options = Object.assign({}, config.requestOptions, {
+                path: config.requestOptions.path + group.join()
             })
 
-            res.on('end', () => {
-                const parsedData = JSON.parse(receivedData[index])
-                const readFunds = parsedData.shift()
-                if (readFunds !== group.length) {
-                    console.error('received fewer funds than requested! ', group, readFunds)
-                }
-                parsedData.forEach((fundData) => {
-                    const fundId = group.shift()
-                    const lastDayData = fundData.pop()
-                    const previousDayData = fundData.pop()
+            requestsInProgress++
 
-                    if (latestDataTimestamp < lastDayData[0]) {
-                        latestDataTimestamp = lastDayData[0]
-                        shouldSendData = true
+            const request = https.request(options, res => {
+                res.on('data', (d) => {
+                    if (receivedData[index] === undefined) {
+                        receivedData[index] = ''
                     }
-                    if (shouldSendData && latestDataTimestamp != lastDayData[0]) {
-                        latestDataTimestamp = Math.min(latestDataTimestamp, lastDayData[0])
+                    receivedData[index] += d
+                })
+
+                res.on('end', () => {
+                    const parsedData = JSON.parse(receivedData[index])
+                    const readFunds = parsedData.shift()
+                    if (readFunds !== group.length) {
+                        console.error('received fewer funds than requested! ', group, readFunds)
+                    }
+                    parsedData.forEach((fundData) => {
+                        const fundId = group.shift()
+                        const lastDayData = fundData.pop()
+                        const previousDayData = fundData.pop()
+
+                        if (latestDataTimestamp < lastDayData[0]) {
+                            latestDataTimestamp = lastDayData[0]
+                            shouldSendData = true
+                        }
+                        if (shouldSendData && latestDataTimestamp != lastDayData[0]) {
+                            latestDataTimestamp = Math.min(latestDataTimestamp, lastDayData[0])
+                            shouldSendData = false
+                        }
+
+                        result.push({
+                            fundId,
+                            name: utils.nameFormat(utils.fundById(fundId).name),
+                            date: utils.dateFormat(new Date(lastDayData[0])),
+                            value: lastDayData[1],
+                            change: utils.calculateChange(lastDayData[1], previousDayData[1])
+                        })
+                    })
+                    requestsInProgress--
+
+                    if (requestsInProgress === 0 && shouldSendData) {
+                        resolve(result)
                         shouldSendData = false
                     }
-
-                    result.push({
-                        fundId,
-                        name: utils.nameFormat(utils.fundById(fundId).name),
-                        date: utils.dateFormat(new Date(lastDayData[0])),
-                        value: lastDayData[1],
-                        change: utils.calculateChange(lastDayData[1], previousDayData[1])
-                    })
                 })
-                requestsInProgress--
-
-                if (requestsInProgress === 0 && shouldSendData) {
-                    sendData(auth, result)
-                    shouldSendData = false
-                }
             })
-        })
 
-        request.end()
+            request.end()
 
-        request.on('error', (e) => {
-            console.error(e)
+            request.on('error', (e) => {
+                reject(e)
+            })
         })
     })
 }
 
 authorize(googleApiKey)
     .then((auth) => {
-        fetchData(auth)
-        const interval = setInterval(() => {fetchData(auth)}, config.requestInterval)
+        console.log('Interval polling started')
+        const interval = setInterval(
+            () => {
+                fetchData()
+                    .then(prepareData)
+                    .then(data => {
+                        console.log('sending data')
+                        return Promise.all[
+                            require('./gmail').send(auth, config, data),
+                            require('./google_sheets').send(auth, config, data)
+                        ]
+                    })
+                    .catch(err => {
+                        clearInterval(interval)
+                        console.log(err)
+                    }
+                )
+            },
+            config.requestInterval
+        )
     })
-    .catch(err => {console.log(err)})
+    .catch(err => {
+        console.log(err)
+    })
+
+
+
